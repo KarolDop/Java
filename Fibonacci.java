5e4f4302d321f0d17c0955636a4eaee3260d9c4b
KarolDop <88980259+KarolDop@users.noreply.github.com>
2022-02-06 22:05:06 +0100

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,47 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Fibonacci
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int sequenceNumber = 0;
+
+        System.out.print("Podaj element ciągu fibonacci'ego którego wartość chcesz poznać: ");
+        try
+        {
+            sequenceNumber = keyboard.nextInt();
+        }
+        catch (InputMismatchException e)
+        {
+            System.out.println("Podałeś inną wartość niż liczba rzeczywista");
+            System.exit(0);
+        }
+        System.out.printf("Ciąg Fibonacci'ego dla %d wynosi: %d", sequenceNumber, fibonacciValue(sequenceNumber));
+
+    }
+
+    public static long fibonacciValue(int sequenceNumber)
+    {
+        long value = 1L;
+        long first = 1L;
+        long second = 1L;
+        long temp;
+
+        if(sequenceNumber == 1 || sequenceNumber == 2)
+            value = 1;
+        else
+        {
+            for(int i = 3; i <= sequenceNumber; i++)
+            {
+                value = first + second;
+                temp = first;
+                first = value;
+                second = temp;
+            }
+        }
+
+        return value;
+    }
+}
